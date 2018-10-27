d0004a0acda5c37f3700c4df3b77aff8d292a349
Robert <robert_luo1949@163.com>
2018-10-27 16:24:43 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# qaplatform
-qa testing platform
+**QA 自动化测试 ---冒烟测试**  <br>
+smoke Test             <br>
+1   编程语言 python 3.6   <br>
+2   开发框架  flask     [官方说明 http://flask.pocoo.org/]  <br>
+3   平台支持  Windows 10 && Centos 7.4   <br>
+
+
+**项目结构介绍**   <br>
+1  smoketest  : 项目测试案例目录 <br>
+ ------------------controller : 控制器和引用libs <br>
+ ------------------static : flask 基础包 静态资源 <br>
+ ------------------templates : flask 基础包 模板 <br>
+ ------------------testcases : 项目的测试案例包  模板 <br>
+ ------------------testdatas : 项目的测试数据包   <br>
+ ------------------testmodel : 项目的测试模型包   <br>
+2  temp  :  保存临时文件目录     <br>
+3  tests :  本项目的测试代码     <br>
+4  venv  :   python3 虚拟机目录 (暂时没有)         <br>
+5  config.py   项目的配置文件[日志][报告][启动参数]  <br>
+6  requirements: 项目的依赖      [导出命令  pip freeze > requirements.txt ] <br>
+7  run.py      项目启动文件 [运行命令  python run.py]   <br>
+
+
+**参考引用:**    <br>
+smoke test 用例清单:  http://confluence.ucex.corp/pages/viewpage.action?pageId=9208286 <br>
+
+新手指导:    <br>
+1     安装pycharm professional 并授权密钥   <br>
+2     配置运行环境  python virtual machine  <br>
+3     git clone 代码 http://gitlab.dev.37touch.com/qa/Qatest_Smoke.git  <br>
+4     到 smoketest指定模块目录下增加  data文件   <br>
+5     编号规则[总共7位数]  前面3位是模块编号  后面4位是测试用例编号,从 101 开始 到 9999 结束. <br>  
---
+++ run.py
@@ -0,0 +1,102 @@
+# coding:utf-8
+'''
+title: 自动化测试 biger
+author:Robert
+date:20180504
+email:shuibo.luo@ucextech.com
+content:  java user-userlogin_101
+other:
+'''
+
+from flask import  Flask
+from flask import render_template
+import config
+import os,sys,json
+import importlib
+from smoketest.controller.libs.email import sendemail
+from smoketest.controller.libs.htmlreport.HTMLTestReportCN import HTMLTestRunner
+from smoketest.controller.log import logger
+from smoketest.testmodel.bigerSmokeTest import modelSuite
+
+
+app = Flask(__name__,static_folder=config.static_dir,template_folder=config.template_dir)
+
+
+testre ="shuibo.luo@ucextech.com,240505723@qq.com"
+@app.route('/')
+
+
+# @app.route('/indextmp')
+#
+# def first_flask():
+#     user = {'nickname' : 'Robert'}
+#     return '''
+#
+# <html>
+#   <head>
+#     <title>Home Page</title>
+#   </head>
+#   <body>
+#     <h1>Hello, ''' + user['nickname'] + '''</h1>
+#   </body>
+# </html>
+# '''
+
+@app.route('/index')
+
+def indextmp():
+    logger.logger.info("testing logger")
+    return render_template("index.html")
+
+
+
+@app.route('/test/smoke',methods=['POST','GET'])
+def testsmoke():
+    logger.logger.info("smoke test")
+
+
+    importlib.reload(config)
+    filePath = config.REPORT_INFO[0]["re_dir"] +config.REPORT_INFO[0]["re_name"]
+    logger.logger.info( "生成的报告路径  " +filePath)
+
+    with open(filePath, mode="wb") as fp:
+        '''
+        #生成报告的Title,描述
+        '''
+        runner =HTMLTestRunner(
+            stream=fp,
+            title=config.REPORT_INFO[0]["re_title"],
+            description=config.REPORT_INFO[0]["re_description"],
+            tester=config.REPORT_INFO[0]["re_author"]
+        )
+        modol = modelSuite()
+        testsuite = modol.SuiteMulti()
+        runner.run(testsuite)
+        # 关闭文件，否则会无法生成文件
+        fp.close()
+        logger.logger.info("smoke test OK  ")
+    result_json = {"Report  dir": filePath,
+                   "Report  url": str(config.HOST_NAME+"/"+config.REPORT_INFO[0]["re_name"])}
+
+    print(fp)
+
+    try:
+        sendemail.sendemailSMTP(
+            config.EMAIL_SERVER[0]["mail_host"],    ##邮件发送SMTP host
+            config.EMAIL_SERVER[0]["mail_user"],    ##邮件发送SMTP  user name
+            config.EMAIL_SERVER[0]["mail_pass"],    ##邮件发送SMTP  user password
+            config.EMAIL_SERVER[0]["receivers"],    ##邮件  收件人
+            config.EMAIL_SERVER[0]["title"],        ##邮件  标题
+            str(json.dumps(result_json))            ##邮件  内容
+        )
+        logger.logger.info(str("FROM  "+config.EMAIL_SERVER[0]["mail_host"] +" To  "+ str(config.EMAIL_SERVER[0]["receivers"])))
+    except BaseException as be:
+        logger.logger.exception("FROM  "+str(config.EMAIL_SERVER[0]["mail_host"]))
+
+
+    return json.dumps(result_json)
+
+
+
+if __name__=='__main__':
+    app.run(host=config.HOST_NAME, port=config.HOST_PORT, debug=config.HOST_DEBUG_MODE,load_dotenv=True)
